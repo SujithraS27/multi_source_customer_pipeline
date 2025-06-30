60be0da2437a644f20e866c3e3ab53f68a5081ed
Sujithra S <sujithra.s2023@vitstudent.ac.in>
2025-07-01 01:27:52 +0530

    Initial commit: Extract step with raw data loading and inspection

---
+++ pipeline.py
@@ -0,0 +1,12 @@
+
+import pandas as pd;
+orders=pd.read_csv(r"E:\DE PROJECT\multi_source_customer_pipeline\bronze\olist_orders_dataset.csv");
+order_items=pd.read_csv(r"E:\DE PROJECT\multi_source_customer_pipeline\bronze\olist_order_items_dataset.csv");
+customers=pd.read_csv(r"E:\DE PROJECT\multi_source_customer_pipeline\bronze\olist_customers_dataset.csv");
+print(orders.head());
+print(orders.info());
+print(order_items.head());
+print(order_items.info());
+print(customers.head());
+print(customers.info());
+
