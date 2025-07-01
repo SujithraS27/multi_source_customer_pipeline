9405edc1fc976bd7ca951a5ba79dba60e293da20
Sujithra S <sujithra.s2023@vitstudent.ac.in>
2025-07-02 00:08:54 +0530

    Cleaned orders data: added delivery_status flag, converted datatypes, saved Silver layer

--- pipeline.py
+++ pipeline.py
@@ -3,7 +3,7 @@ import pandas as pd;
 orders=pd.read_csv(r"E:\DE PROJECT\multi_source_customer_pipeline\bronze\olist_orders_dataset.csv");
 order_items=pd.read_csv(r"E:\DE PROJECT\multi_source_customer_pipeline\bronze\olist_order_items_dataset.csv");
 customers=pd.read_csv(r"E:\DE PROJECT\multi_source_customer_pipeline\bronze\olist_customers_dataset.csv");
-'''print(orders.head());
+print(orders.head());
 print(orders.info());
 print(order_items.head());
 print(order_items.info());
@@ -15,10 +15,20 @@ print(customers.isnull().sum())
 print(orders.duplicated().sum())
 print(customers.duplicated().sum())
 print(order_items.duplicated().sum())
-print(orders.describe())'''
+print(orders.describe())
 orders['delivery_status'] = orders['order_delivered_customer_date'].notnull()
-print(orders.order_delivered_carrier_date)
+print(orders.order_id)
+print(customers.customer_unique_id)
+print(order_items.seller_id)
+orders['order_purchase_timestamp'] = pd.to_datetime(orders['order_purchase_timestamp'])
+orders['order_approved_at'] = pd.to_datetime(orders['order_approved_at'])
+orders['order_delivered_carrier_date'] = pd.to_datetime(orders['order_delivered_carrier_date'])
+orders['order_delivered_customer_date'] = pd.to_datetime(orders['order_delivered_customer_date'])
+orders['order_estimated_delivery_date'] = pd.to_datetime(orders['order_estimated_delivery_date'])
+print(orders.dtypes)
+print(customers.dtypes)
+order_items['shipping_limit_date'] = pd.to_datetime(order_items['shipping_limit_date'])
+print(order_items.dtypes)
 orders.to_csv("E:/DE PROJECT/multi_source_customer_pipeline/silver/orders_cleaned.csv", index=False)
 customers.to_csv("E:/DE PROJECT/multi_source_customer_pipeline/silver/customers.csv", index=False)
 order_items.to_csv("E:/DE PROJECT/multi_source_customer_pipeline/silver/order_items.csv", index=False)
-
